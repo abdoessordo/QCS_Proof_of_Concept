a9c7f2e018c66dc5f05770870451e5691825af7d
Essordo <223131678@ge.com>
2024-03-06 12:25:47 +0100

    some code refactoring

--- src/main/java/com/ge/proof_of_concept/user/users/UsersController.java
+++ src/main/java/com/ge/proof_of_concept/user/users/UsersController.java
@@ -1,51 +1,44 @@
 package com.ge.proof_of_concept.user.users;
 
 
-import com.ge.proof_of_concept._backup.users_history.UsersHistory;
-import com.ge.proof_of_concept._backup.users_history.UsersHistoryService;
-import com.ge.proof_of_concept.util.HistoryAction;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
-import java.time.LocalDateTime;
 import java.util.List;
 
 /**
  * This class is a controller for the Users entity
  * It handles the HTTP requests and responses for the Users entity
- * <p>
+ *
  * Endpoints:
  * GET /api/v1/users - returns a list of all users
  * GET /api/v1/users/deleted - returns a list of all soft deleted users
  * POST /api/v1/users/add - adds a new user to the database
  * DELETE /api/v1/users/{SSO} - deletes a user from the database
  * POST /api/v1/users/{SSO}/restore - restores a deleted user from the database
- * <p>
+ *
  * Author: Abdellah ESSORDO
- * Created on: 03/06/2024
+ * Created on: 03/03/2024
  */
-
 @RestController
 @RequestMapping(path = "api/v1/users")
 public class UsersController {
+
     /**
      * TODO:
-     *  updated_by should be the user who is currently logged in, currently it is the user who is being restored [ ]
+     *      Return JSON response for all the endpoints [ ]
      */
 
     private final UsersService usersService;
-    private final UsersHistoryService usersHistoryService;
-
     @Autowired
-    public UsersController(UsersService usersService, UsersHistoryService usersHistoryService) {
+    public UsersController(UsersService usersService) {
         this.usersService = usersService;
-        this.usersHistoryService = usersHistoryService;
     }
 
 
     /**
      * GET /api/v1/users
-     * <p>
+     *
      * This method returns a list of all users
      *
      * @param void
@@ -58,7 +51,7 @@ public class UsersController {
 
     /**
      * POST /api/v1/users/add
-     * <p>
+     *
      * This method adds a new user to the database,
      * and also adds a new entry to the users_history table
      *
@@ -67,32 +60,12 @@ public class UsersController {
      */
     @PostMapping(path = "/add")
     public void addNewUser(@RequestBody Users user) {
-        /**
-         * TODO:
-         *  Add validation for the user object [ ]
-         *  Check if the user already exists [ ]
-         *  Add the user to the users table [x]
-         *  Add the user to the users_history table [x]
-         */
-
-        // Add the user to the users table
         usersService.addNewUser(user);
-
-        // Add the user to the users_history table
-        usersHistoryService.addNewUserHistory(
-                new UsersHistory(
-                        user,
-                        user.getName(),
-                        user, // updated_by, Should be the user who is currently logged in
-                        LocalDateTime.now(),
-                        HistoryAction.CREATED
-                )
-        );
     }
 
     /**
      * DELETE /api/v1/users/{userId}
-     * <p>
+     *
      * This method soft deletes a user from the database,
      * and also adds a new entry to the users_history table
      *
@@ -101,45 +74,13 @@ public class UsersController {
      */
     @DeleteMapping(path = "{SSO}")
     public void deleteUser(@PathVariable("SSO") Long SSO) {
-        /**
-         * TODO:
-         *  Check if the user exists [x]
-         *  Check if the user is already deleted [x]
-         *  Soft delete the user from the users table [x]
-         *  Add the user to the users_history table [x]
-         */
-
-        // Check if the user exists
-        boolean exists = usersService.existsById(SSO);
-        if (!exists) {
-            throw new IllegalStateException("user with id " + SSO + " does not exist");
-        }
-
-        // Check if the user is already deleted
-        Users user = usersService.findById(SSO);
-        if (user.getDeletedAt() != null) {
-            throw new IllegalStateException("user with id " + SSO + " is already deleted");
-        }
-
-        // Soft delete the user from the users table
         usersService.deleteUser(SSO);
-
-        // Add the user to the users_history table
-        usersHistoryService.addNewUserHistory(
-                new UsersHistory(
-                        user,
-                        user.getName(),
-                        user, // updated_by, Should be the user who is currently logged in
-                        LocalDateTime.now(),
-                        HistoryAction.DELETED
-                )
-        );
-
     }
+
 
     /**
      * POST /api/v1/users/{SSO}/restore
-     * <p>
+     *
      * This method restores a user from the database,
      * and also adds a new entry to the users_history table
      *
@@ -147,32 +88,19 @@ public class UsersController {
      * @return void
      */
     @PostMapping(path = "{SSO}/restore")
-    public void restoreUser(@PathVariable("SSO") Long SSO) {
-        /**
-         * TODO:
-         *  Check if the user exists [x]
-         *  Check if the user is already restored [x]
-         *  Restore the user from the users table [x]
-         *  Add the user to the users_history table [x]
-         */
-
-        // Check if the user exists
-        boolean exists = usersService.existsById(SSO);
-        if (!exists) {
-            throw new IllegalStateException("user with id " + SSO + " does not exist");
-        }
-
-        // Check if the user is already restored
-        Users user = usersService.findById(SSO);
-        if (user.getDeletedAt() == null) {
-            throw new IllegalStateException("user with id " + SSO + " is already restored");
-        }
-
-        // Restore the user from the users table
-        usersService.restoreUser(user);
+    public void restoreUserBySSO(@PathVariable("SSO") Long SSO) {
+        usersService.restoreUser(SSO);
 
     }
 
+    /**
+     * GET /api/v1/users/deleted
+     *
+     * This method returns a list of all soft deleted users
+     *
+     * @param void
+     * @return List<Users> - a list of all soft deleted users
+     */
     @GetMapping(path = "/deleted")
     public List<Users> getAllSoftDeletedUsers() {
         return usersService.getSoftDeletedUsers();
--- src/main/java/com/ge/proof_of_concept/user/users/UsersService.java
+++ src/main/java/com/ge/proof_of_concept/user/users/UsersService.java
@@ -13,8 +13,28 @@ import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Optional;
 
+
+/**
+ * This class is a service for the Users entity
+ * It manages the Users entity in the database and provides methods for CRUD operations
+ *
+ * Methods:
+ *  - getUsers
+ *  - addNewUser
+ *  - deleteUser
+ *  - getSoftDeletedUsers
+ *  - restoreUser
+ *
+ *  Author: Abdellah ESSORDO
+ *  Created on: 03/03/2024
+ */
 @Service
 public class UsersService {
+
+    /**
+     * TODO:
+     *  updated_by should be the user who is currently logged in, currently it is the user who is being restored [ ]
+     */
 
     private final UsersRepository usersRepository;
     private final UsersHistoryService usersHistoryService;
@@ -27,58 +47,147 @@ public class UsersService {
         this.entityManager = entityManager;
     }
 
-
+    /**
+     * This method returns a list of non-soft deleted users
+     * @return List<Users>
+     */
     public List<Users> getUsers() {
+        // The two lines below are used to apply the deleted filter to the users table,
+        // so that only the non-soft deleted users are returned
         Session session = entityManager.unwrap(Session.class);
         FilterUtil.applyDeletedFilter(session, Users.class);
 
         return usersRepository.findAll();
     }
 
-    public void addNewUser(Users user) {
-        usersRepository.save(user);
-    }
-
-    public boolean existsById(Long userId) {
-        return usersRepository.existsById(userId);
-    }
-
-    public Users findById(Long userId) {
-        return usersRepository.findById(userId).orElse(null);
-    }
-
-    public void deleteUser(Long userId) {
-        usersRepository.deleteById(userId);
-    }
-
+    /**
+     * This method returns a list of soft deleted users
+     * @return List<Users>
+     */
     public List<Users> getSoftDeletedUsers() {
         return usersRepository.findByDeletedAtIsNotNull();
     }
 
-
-    public void restoreUser(Users user) {
+    /**
+     * This method is used to add a new user to the database
+     * @param user: Users
+     * @return void
+     */
+    public void addNewUser(Users user) {
         /**
-         * User assumed that exists by the time this method is called
-         *
          * TODO:
-         *  - set deleted_at to null
-         *  - add a new entry to the users_history table
+         *  Add validation for the user object [ ]
+         *  Check if the user already exists [ ]
+         *  Add the user to the users table [x]
+         *  Add the user to the users_history table [x]
          */
 
-        // set deleted_at to null
-        user.setDeletedAt(null);
+        // Add the user to the users table
         usersRepository.save(user);
 
-        // add a new entry to the users_history table
+        // Add the user to the users_history table
         usersHistoryService.addNewUserHistory(
                 new UsersHistory(
                         user,
                         user.getName(),
                         user, // updated_by, Should be the user who is currently logged in
                         LocalDateTime.now(),
-                        HistoryAction.RESTORED
+                        HistoryAction.CREATED
                 )
         );
+    }
 
+
+    /**
+     * This method is used to delete a user from the database
+     * Checks if the user exists and if it is already deleted, then soft deletes the user
+     *
+     * @param SSO: Long
+     * @return void
+     *
+     * @throws IllegalStateException if the user with the given id does not exist
+     */
+    public void deleteUser(Long SSO) {
+        // Check if the user exists
+        boolean exists = usersRepository.existsById(SSO);
+        if (!exists) {
+            throw new IllegalStateException("user with id " + SSO + " does not exist");
+        }
+
+        // Check if the user is soft deleted
+        Optional<Users> userOptional = usersRepository.findById(SSO);
+        if (userOptional.isPresent()) {
+            Users user = userOptional.get();
+
+            if (user.getDeletedAt() != null) {
+                throw new IllegalStateException("user with id " + SSO + " is already deleted");
+            }
+
+            // Soft delete the user from the users table
+            usersRepository.deleteById(SSO);
+
+            // Add the user to the users_history table
+            usersHistoryService.addNewUserHistory(
+                    new UsersHistory(
+                            user,
+                            user.getName(),
+                            user, // updated_by, Should be the user who is currently logged in
+                            LocalDateTime.now(),
+                            HistoryAction.DELETED
+                    )
+            );
+
+        }
+    }
+
+
+    /**
+     * This method is used to restore a soft deleted user from the database
+     * @param SSO
+     * @return void
+     */
+    public void restoreUser(Long SSO) {
+        /**
+         * TODO:
+         *  Check if the user exists [x]
+         *  Check if the user is already restored [x]
+         *  Restore the user from the users table [x]
+         *  Add the user to the users_history table [x]
+         *  set deleted_at to null
+         *  add a new entry to the users_history table
+         */
+
+        // Check if the user exists
+        boolean exists = usersRepository.existsById(SSO);
+        if (!exists) {
+            throw new IllegalStateException("user with id " + SSO + " does not exist");
+        }
+
+        // Check if the user is already restored
+        Optional<Users> userOptional = usersRepository.findById(SSO);
+        if (userOptional.isPresent()) {
+            Users user = userOptional.get();
+
+            if (user.getDeletedAt() == null) {
+                throw new IllegalStateException("user with id " + SSO + " is already restored");
+            }
+
+            // Restore the user from the users table
+            // set deleted_at to null
+            user.setDeletedAt(null);
+            usersRepository.save(user);
+
+            // add a new entry to the users_history table
+            usersHistoryService.addNewUserHistory(
+                    new UsersHistory(
+                            user,
+                            user.getName(),
+                            user, // updated_by, Should be the user who is currently logged in
+                            LocalDateTime.now(),
+                            HistoryAction.RESTORED
+                    )
+            );
+
+        }
     }
 }
